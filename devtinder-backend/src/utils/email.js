df721cd5c3e08f718b0824e53009f93478d62028
Navneet Rana <rnavneet54@gmail.com>
2025-03-12 00:56:15 +0530

    bug fix

--- devtinder-backend/src/utils/email.js
+++ devtinder-backend/src/utils/email.js
@@ -1,5 +1,5 @@
 const {SendEmailCommand} = require("@aws-sdk/client-ses");
-const {sesclient} = require("./sesclient");
+const {client} = require("./sesclient");
 
 const createSendEmailCommand = (toAddress, fromAddress, subject, body) => {
     return new SendEmailCommand({
@@ -46,7 +46,7 @@ const createSendEmailCommand = (toAddress, fromAddress, subject, body) => {
     );
   
     try {
-      return await sesclient.send(sendEmailCommand);
+      return await client.send(sendEmailCommand);
     } catch (caught) {
       if (caught instanceof Error && caught.name === "MessageRejected") {
         /** @type { import('@aws-sdk/client-ses').MessageRejected} */
--- devtinder-backend/src/utils/sesClient.js
+++ devtinder-backend/src/utils/sesClient.js
@@ -2,9 +2,9 @@ const { SESClient } = require("@aws-sdk/client-ses");
 // Set the AWS Region.
 const REGION = "ap-south-1"; //for mumbai
 // Create SES service object.
-const sesclient = new SESClient({ region: REGION, credentials:{
+const client = new SESClient({ region: REGION, credentials:{
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_KEY
 } });
-module.exports = { sesclient };
+module.exports = { client };
 // snippet-end:[ses.JavaScript.createclientv3]
